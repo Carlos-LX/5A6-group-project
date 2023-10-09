cef18267bc961b3efa003bc735b7ada6b8afbecb
Carlos <celx@loxi.ca>
2023-10-09 17:06:23 -0400

    Added ktlint to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,15 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "7.1.0" apply false
 }
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
+    }
+}
+
