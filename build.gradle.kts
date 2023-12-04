40601374db96226c0fa6faa9d4e6f71514948ba6
Carlos <2153301@johnabbottcollege.net>
2023-12-04 16:48:33 -0500

    changed ktlint version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id ("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+    id ("org.jlleitschuh.gradle.ktlint") version "12.0.2"
 }
 allprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
