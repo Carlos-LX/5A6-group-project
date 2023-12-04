832b9ba94b084390ba08bf1f5f9ed4176e385dd9
Carlos <2153301@johnabbottcollege.net>
2023-12-04 16:42:53 -0500

    added ktlint plugin to try to pull changes into main

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id ("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+}
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
